bb72114cb5b4a8c511e4627059bd1e2d8df159e4
Mikka <mikka@sendke.eu>
2023-12-21 21:07:20 +0100

    name color logging + idk fmt or something

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::env;
 use serde::Serialize;
-use twitch_irc::{ ClientConfig, SecureTCPTransport, TwitchIRCClient, message::ServerMessage, login::StaticLoginCredentials };
+use std::env;
+use twitch_irc::{
+    login::StaticLoginCredentials, message::ServerMessage, ClientConfig, SecureTCPTransport,
+    TwitchIRCClient,
+};
 
 #[derive(Debug, Serialize)]
 struct ChatMessage {
@@ -11,11 +14,10 @@ struct ChatMessage {
     pub display_name: String,
 }
 
-
 #[tokio::main]
 async fn main() {
     let url = "https://api.op47.de/v1/twitch/insertMessage";
-    let look_in = vec!["stegi", "di1araas"]; 
+    let look_in = vec!["stegi", "di1araas"];
     let look_for = vec!["stegi", "di1araas"];
 
     let api_key = env::var("API_KEY").expect("API_KEY must be set");
@@ -32,12 +34,13 @@ async fn main() {
     }
 
     let config = ClientConfig::default();
-    let (mut incoming_messages, client) = TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
+    let (mut incoming_messages, client) =
+        TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
 
     let join_handle = tokio::spawn(async move {
         while let Some(message) = incoming_messages.recv().await {
             handle_message(message, &look_for, &url, &api_key).await;
-        }    
+        }
     });
 
     for channel in look_in {
@@ -48,6 +51,7 @@ async fn main() {
     }
     join_handle.await.unwrap();
 }
+
 async fn handle_message(message: ServerMessage, look_for: &Vec<&str>, url: &str, api_key: &str) {
     match message {
         ServerMessage::Privmsg(msg) => {
@@ -55,22 +59,34 @@ async fn handle_message(message: ServerMessage, look_for: &Vec<&str>, url: &str,
                 let chat_message = ChatMessage {
                     timestamp: msg.server_timestamp.timestamp(),
                     channel: "#".to_string() + msg.channel_login.as_str(),
-                    user: msg.sender.login,
+                    user: msg.sender.login.clone(),
                     content: msg.message_text,
                     display_name: msg.sender.name,
                 };
+                let color = msg.name_color.unwrap();
+                println!(
+                    "user: {0}; rgb: ({1}, {2}, {3})",
+                    &msg.sender.login.as_str(),
+                    color.r,
+                    color.g,
+                    color.b
+                );
+
                 match post_message(chat_message, url, api_key).await {
                     Ok(_) => println!("Message posted"),
                     Err(e) => println!("Error posting message: {}", e),
                 }
             }
-        },
-        _ => {
         }
-    }
+        _ => {}
+    };
 }
 
-async fn post_message(message: ChatMessage, url: &str, api_key: &str) -> Result<(), reqwest::Error> {
+async fn post_message(
+    message: ChatMessage,
+    url: &str,
+    api_key: &str,
+) -> Result<(), reqwest::Error> {
     let http_client = reqwest::Client::new();
     let body = serde_json::to_string_pretty(&message).unwrap();
     let _ = http_client
